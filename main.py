b7d6162ca9ec033c2490be597a000028e38857c5
palbanowski <pawel.albanowski@gmail.com>
2022-01-28 14:28:29 +0100

    add user_sort.py, move user sorting logic into user_sort.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from requests import sessions
 from pprint import pprint
-from ldap3 import SUBTREE
 import time
 from func import system_time, json_read, json_write, log_append
 from general_message import general_message
-from classes import Teams
 from connections import Connections
+from user_sort import user_sort
 
 with sessions.Session() as session:
     rocket = Connections.rocket
@@ -18,86 +17,7 @@ with sessions.Session() as session:
             jsonData['totalUsers'] = total
             for user in userList:
                 if user['roles'] == ['user']:  # or user['roles'] == ['user', 'guest']:
-                    conn = Connections.conn
-                    searchFilter = '(&(objectclass=user)(sAMAccountName=' + user['username'] + '))'
-                    status, result, response, _ = conn.search(
-                        search_base='OU=Szwajcarska,DC=szpitalsm,DC=local',
-                        search_filter=searchFilter,
-                        attributes=['sAMAccountName'],
-                        search_scope=SUBTREE)
-                    for ldapUser in response:
-                        dnIT = ldapUser['dn'].rfind(Teams.it.getDn())
-                        dnPlace = ldapUser['dn'].rfind(Teams.place.getDn())
-                        dnWew = ldapUser['dn'].rfind(Teams.wew.getDn())
-                        dnKadry = ldapUser['dn'].rfind(Teams.kadry.getDn())
-                        dnKsiegowosc = ldapUser['dn'].rfind(Teams.ksiegowosc.getDn())
-                        dnDNM = ldapUser['dn'].rfind(Teams.dnm.getDn())
-                        dnDLA = ldapUser['dn'].rfind(Teams.dla.getDn())
-                        dnInwentaryzacja = ldapUser['dn'].rfind(Teams.inwentaryzacja.getDn())
-                        dnZaopatrzenie = ldapUser['dn'].rfind(Teams.zaopatrzenie.getDn())
-                        dnOrgPrawny = ldapUser['dn'].rfind(Teams.orgprawny.getDn())
-                        if dnIT != -1:
-                            rocket.groups_invite(Teams.it.getId(), user['_id'])
-                            log = system_time() + ' - Added ' + user['username'] + ' to IT'
-                            pprint(log)
-                            log_append('log.txt', log)
-                            # rocket.users_update(user['_id'], nickname='IT')
-                        if dnPlace != -1:
-                            rocket.groups_invite(Teams.place.getId(), user['_id'])
-                            log = system_time() + ' - Added ' + user['username'] + ' to Place'
-                            pprint(log)
-                            log_append('log.txt', log)
-                            # rocket.users_update(user['_id'], nickname='Płace')
-                        if dnWew != -1:
-                            rocket.groups_invite(Teams.wew.getId(), user['_id'])
-                            log = system_time() + ' - Added ' + user['username'] + ' to Wewnetrzny'
-                            pprint(log)
-                            log_append('log.txt', log)
-                            # rocket.users_update(user['_id'], nickname='Oddział Wewnętrzny')
-                        if dnKadry != -1:
-                            rocket.groups_invite(Teams.kadry.getId(), user['_id'])
-                            log = system_time() + ' - Added ' + user['username'] + ' to Kadry'
-                            pprint(log)
-                            log_append('log.txt', log)
-                            # rocket.users_update(user['_id'], nickname='Kadry')
-                        if dnKsiegowosc != -1:
-                            rocket.groups_invite(Teams.ksiegowosc.getId(), user['_id'])
-                            log = system_time() + ' - Added ' + user['username'] + ' to Ksiegowosc'
-                            pprint(log)
-                            log_append('log.txt', log)
-                            # rocket.users_update(user['_id'], nickname='Ksiegowosc')
-                        if dnDNM != -1:
-                            rocket.groups_invite(Teams.dnm.getId(), user['_id'])
-                            log = system_time() + ' - Added ' + user['username'] + ' to NadzorMedyczny'
-                            pprint(log)
-                            log_append('log.txt', log)
-                            # rocket.users_update(user['_id'], nickname='NadzorMedyczny')
-                        if dnDLA != -1:
-                            rocket.groups_invite(Teams.dla.getId(), user['_id'])
-                            log = system_time() + ' - Added ' + user['username'] + ' to LogistycznoAdministracyjny'
-                            pprint(log)
-                            log_append('log.txt', log)
-                            # rocket.users_update(user['_id'], nickname='LogistycznoAdministracyjny')
-                        if dnInwentaryzacja != -1:
-                            rocket.groups_invite(Teams.inwentaryzacja.getId(), user['_id'])
-                            log = system_time() + ' - Added ' + user['username'] + ' to Inwentaryzacja'
-                            pprint(log)
-                            log_append('log.txt', log)
-                            # rocket.users_update(user['_id'], nickname='Inwentaryzacja')
-                        if dnZaopatrzenie != -1:
-                            rocket.groups_invite(Teams.zaopatrzenie.getId(), user['_id'])
-                            log = system_time() + ' - Added ' + user['username'] + ' to Zaopatrzenie'
-                            pprint(log)
-                            log_append('log.txt', log)
-                            # rocket.users_update(user['_id'], nickname='Zaopatrzenie')
-                        if dnOrgPrawny != -1:
-                            rocket.groups_invite(Teams.orgprawny.getId(), user['_id'])
-                            log = system_time() + ' - Added ' + user['username'] + ' to OrganizacyjnoPrawny'
-                            pprint(log)
-                            log_append('log.txt', log)
-                            # rocket.users_update(user['_id'], nickname='OrganizacyjnoPrawny')
-                    rocket.users_update(user['_id'], roles=['user', 'guest'])
-                    rocket.channels_invite('KwWXAxZp9E7tEzygt', user['_id'])
+                    user_sort(rocket, user)
             msg_log = general_message(rocket)
             pprint(msg_log)
             log_append('log.txt', msg_log)
---
+++ user_sort.py
@@ -0,0 +1,92 @@
+from requests import sessions
+from pprint import pprint
+from ldap3 import SUBTREE
+import time
+from func import system_time, json_read, json_write, log_append
+from general_message import general_message
+from classes import Teams
+from connections import Connections
+
+
+def user_sort(rocket, user):
+    conn = Connections.conn
+    search_filter = '(&(objectclass=user)(sAMAccountName=' + user['username'] + '))'
+    status, result, response, _ = conn.search(
+        search_base='OU=Szwajcarska,DC=szpitalsm,DC=local',
+        search_filter=search_filter,
+        attributes=['sAMAccountName'],
+        search_scope=SUBTREE)
+    for ldapUser in response:
+        dn_it = ldapUser['dn'].rfind(Teams.it.getDn())
+        dn_place = ldapUser['dn'].rfind(Teams.place.getDn())
+        dn_wew = ldapUser['dn'].rfind(Teams.wew.getDn())
+        dn_kadry = ldapUser['dn'].rfind(Teams.kadry.getDn())
+        dn_ksiegowosc = ldapUser['dn'].rfind(Teams.ksiegowosc.getDn())
+        dn_dnm = ldapUser['dn'].rfind(Teams.dnm.getDn())
+        dn_dla = ldapUser['dn'].rfind(Teams.dla.getDn())
+        dn_inwentaryzacja = ldapUser['dn'].rfind(Teams.inwentaryzacja.getDn())
+        dn_zaopatrzenie = ldapUser['dn'].rfind(Teams.zaopatrzenie.getDn())
+        dn_orgprawny = ldapUser['dn'].rfind(Teams.orgprawny.getDn())
+        if dn_it != -1:
+            rocket.groups_invite(Teams.it.getId(), user['_id'])
+            log = system_time() + ' - Added ' + user['username'] + ' to IT'
+            pprint(log)
+            log_append('log.txt', log)
+            # rocket.users_update(user['_id'], nickname='IT')
+        if dn_place != -1:
+            rocket.groups_invite(Teams.place.getId(), user['_id'])
+            log = system_time() + ' - Added ' + user['username'] + ' to Place'
+            pprint(log)
+            log_append('log.txt', log)
+            # rocket.users_update(user['_id'], nickname='Płace')
+        if dn_wew != -1:
+            rocket.groups_invite(Teams.wew.getId(), user['_id'])
+            log = system_time() + ' - Added ' + user['username'] + ' to Wewnetrzny'
+            pprint(log)
+            log_append('log.txt', log)
+            # rocket.users_update(user['_id'], nickname='Oddział Wewnętrzny')
+        if dn_kadry != -1:
+            rocket.groups_invite(Teams.kadry.getId(), user['_id'])
+            log = system_time() + ' - Added ' + user['username'] + ' to Kadry'
+            pprint(log)
+            log_append('log.txt', log)
+            # rocket.users_update(user['_id'], nickname='Kadry')
+        if dn_ksiegowosc != -1:
+            rocket.groups_invite(Teams.ksiegowosc.getId(), user['_id'])
+            log = system_time() + ' - Added ' + user['username'] + ' to Ksiegowosc'
+            pprint(log)
+            log_append('log.txt', log)
+            # rocket.users_update(user['_id'], nickname='Ksiegowosc')
+        if dn_dnm != -1:
+            rocket.groups_invite(Teams.dnm.getId(), user['_id'])
+            log = system_time() + ' - Added ' + user['username'] + ' to NadzorMedyczny'
+            pprint(log)
+            log_append('log.txt', log)
+            # rocket.users_update(user['_id'], nickname='NadzorMedyczny')
+        if dn_dla != -1:
+            rocket.groups_invite(Teams.dla.getId(), user['_id'])
+            log = system_time() + ' - Added ' + user['username'] + ' to LogistycznoAdministracyjny'
+            pprint(log)
+            log_append('log.txt', log)
+            # rocket.users_update(user['_id'], nickname='LogistycznoAdministracyjny')
+        if dn_inwentaryzacja != -1:
+            rocket.groups_invite(Teams.inwentaryzacja.getId(), user['_id'])
+            log = system_time() + ' - Added ' + user['username'] + ' to Inwentaryzacja'
+            pprint(log)
+            log_append('log.txt', log)
+            # rocket.users_update(user['_id'], nickname='Inwentaryzacja')
+        if dn_zaopatrzenie != -1:
+            rocket.groups_invite(Teams.zaopatrzenie.getId(), user['_id'])
+            log = system_time() + ' - Added ' + user['username'] + ' to Zaopatrzenie'
+            pprint(log)
+            log_append('log.txt', log)
+            # rocket.users_update(user['_id'], nickname='Zaopatrzenie')
+        if dn_orgprawny != -1:
+            rocket.groups_invite(Teams.orgprawny.getId(), user['_id'])
+            log = system_time() + ' - Added ' + user['username'] + ' to OrganizacyjnoPrawny'
+            pprint(log)
+            log_append('log.txt', log)
+            # rocket.users_update(user['_id'], nickname='OrganizacyjnoPrawny')
+    rocket.users_update(user['_id'], roles=['user', 'guest'])
+    rocket.channels_invite('KwWXAxZp9E7tEzygt', user['_id'])
+    return
