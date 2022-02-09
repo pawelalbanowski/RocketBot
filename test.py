8e4027f3da7038dea32f169a3709bf59247fb5df
Paweł Albanowski <50399152+pawelalbanowski@users.noreply.github.com>
2022-02-09 12:59:26 +0100

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from classes import Teams
-from pprint import pprint
-
-teams = Teams()
-for attr, value in teams.__dict__.items():
-    pprint(attr, )
-teams = [a for a in dir(teams) if not a.startswith('__')]
-
