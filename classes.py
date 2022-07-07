3fb6a5dcf25777f52184af1773065b60ef8f7d73
Paweł Albanowski <50399152+pawelalbanowski@users.noreply.github.com>
2022-07-07 10:26:15 +0200

    prt

--- classes.py
+++ classes.py
@@ -37,44 +37,65 @@ class Team:
 class Teams:
     it = Team('mqayX5ocpq39kEwyC', 'IT', 'mdomanski', 'OU=ITPracownicy',
               'INFORMATYKA', 'administracja')
+    
     wew = Team('HnS2sggDGCCwik3iz', 'oddzialchorobwewnetrznych', 'mzytkiewicz', 'OU=Wewnetrzny',
                'ODDZIAŁ CHORÓB WEWNĘTRZNYCH', 'szpital')
+    
     place = Team('2mPqikSv6JhYm8Dc7', 'place', 'abronczyk', 'OU=Place',
                  'PŁACE', 'administracja')
+    
     kadry = Team('CMKTnYtyGgGBeLer3', 'Kadry', 'mkaczmarek', 'OU=Kadry',
                  'KADRY', 'administracja')
+    
     ksiegowosc = Team('gFKkoAeaNs8rz5gji', 'ksiegowosc', 'rpiatek', 'OU=Ksiegowosc',
                       'KSIĘGOWOŚĆ', 'administracja')
+    
     dnm = Team('Gqvuu45Wg4HEhgbXH', 'NadzorMedyczny', 'jbrojewska', 'OU=NadzorMedyczny',
                'NADZÓR MEDYCZNY', 'administracja')
+    
     dla = Team('n7dRyCDmZbNjNHYLM', 'logistycznoadministracyjny', 'dmajchrzak', 'OU=LogistykaAdministracja',
                'LOGISTYCZNO-ADMINISTRACYJNY', 'administracja')
+    
     inwentaryzacja = Team('5QsZK9s8YC6224H8b', 'inwentaryzacja', 'ddabrowski', 'OU=Inwentaryzacja',
                           'INWENTARYZACJA', 'administracja')
+    
     zaopatrzenie = Team('2fipuxzeMMXwcASgv', 'zaopatrzenie', 'rszumacher', 'OU=Zaopatrzenie',
                         'ZAOPATRZENIE', 'administracja')
+    
     orgprawny = Team('uAdQPzEkzMCrdpD8S', 'organizacyjnoprawny', 'anowotarska', 'OU=Organizacja',
                      'ORGANIZACYJNO-PRAWNY', 'administracja')
+    
     zamowienia = Team('MEckW2gTE8KDXM6GK', 'zamowienia', 'ajackowiak', 'OU=Zamowienia',
                       'ZAMÓWIENIA PUBLICZNE', 'administracja')
+    
     dyrekcja = Team('8hhTD9nqMhXHBxGLx', 'dyrekcja', 'bgruszka', 'OU=Dyrekcja',
                     'DYREKCJA', 'administracja')
+    
     akredytacja = Team('8hhTD9nqMhXHBxGLx', 'dyrekcja', 'vmatecka', 'OU=Akredytacja',
                        'AKREDYTACJA', 'administracja')
+    
     techniczny = Team('nn4EPzpefbn6qGJNK', 'dzialtechniczny', 'lgalkowski', 'OU=DzialTechniczny',
                       'TECHNICZNO-EKSPLOATACYJNY', 'administracja')
+    
     apteka = Team('bYRdJrXYLEEcfqj5j', 'apteka', 'dkurasz', 'OU=Apteka', 'APTEKA', 'szpital')
+    
     iso = Team('dtsfQ5PwQBCW97uXN', 'iso', 'ekapecka', 'OU=ISO', 'ISO', 'administracja')
+    
     inwestycje = Team('KCX9W59CMMjh7mepZ', 'inwestycje', 'pkachel', 'OU=Inwestycje',
                       'INWESTYCJE', 'administracja')
+    
     epidemiologia = Team('uMjnyfuAP5pyimfEq', 'epidemiologia', '', 'OU=PielEpidemiologiczna',
                          'EPIDEMIOLOGIA', 'administracja')
+    
     bhp = Team('kWauhZNtjLg2RgF7n', 'bhp', 'mmaciejewski', 'OU=Bhp',
                'BHP', 'administracja')
+    
     fizykoterapia = Team('2epAzHvmKMeqWMvaw', 'fizykoterapia', 'edera', 'OU=Fizykoterapia',
                          'FIZYKOTERAPIA', 'szpital')
+    
     labo = Team('baHp67Fm3RAoTt4br', 'laboratorium', 'mlisiecka', 'OU=Laboratorium',
                 'LABORATORIUM', 'szpital')
+    
     teams = [it, wew, place, kadry, ksiegowosc, dnm, dla, inwentaryzacja,
              zaopatrzenie, orgprawny, zamowienia, dyrekcja, akredytacja, techniczny, apteka,
              inwestycje, iso, epidemiologia, bhp, fizykoterapia, labo]
