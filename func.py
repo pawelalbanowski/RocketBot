a73a51b666eb32baf6128bfb31e5d4b6688c2582
palbanowski <pawel.albanowski@gmail.com>
2022-09-07 14:20:15 +0200

    file error handling

--- func.py
+++ func.py
@@ -10,25 +10,32 @@ def system_time():
 
 
 def json_read(file):
-    read_json_file = open(file, 'r')
-    json_data = json.load(read_json_file)
-    read_json_file.close()
-    return json_data
+    try:
+        read_json_file = open(file, 'r')
+        json_data = json.load(read_json_file)
+        read_json_file.close()
+        return json_data
+    except FileNotFoundError as err:
+        print(err)
 
 
 def json_write(file, data):
-    write_json = json.dumps(data, indent=4)
-    write_file = open(file, 'w')
-    write_file.write(write_json)
-    write_file.close()
-    return
+    try:
+        write_json = json.dumps(data, indent=4)
+        write_file = open(file, 'w')
+        write_file.write(write_json)
+        write_file.close()
+    except FileNotFoundError as err:
+        print(err)
 
 
 def log_append(data):  # append to log file
-    f = open(LOG_FILE, 'a')
-    f.write(data + '\n')
-    f.close()
-    return
+    try:
+        f = open(LOG_FILE, 'a')
+        f.write(data + '\n')
+        f.close()
+    except FileNotFoundError as err:
+        print(err)
 
 
 def presence_translate(data):
